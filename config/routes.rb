a71791247cb038bd4935bed17755fbe7b2f1466a
Tony <ANCLIST@GMAIL.COM>
2017-10-20 16:31:13 -0400

    Log In

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'pictures#index'
 resources :pictures
 resources :users, only: [:new, :create]
+resources :sessions, only: [:new, :create, :destroy]
 end

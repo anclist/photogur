2e7f71cc69c4a424e37a66fa7f4e2a3669389f76
Tony <ANCLIST@GMAIL.COM>
2017-10-20 15:49:54 -0400

    Create a new user, view and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'pictures#index'
-get 'pictures' => 'pictures#index'
-
-post 'pictures' => 'pictures#create'
-get 'pictures/new' => 'pictures#new'
-
-get 'pictures/:id/edit' => "pictures#edit"
-patch 'pictures/:id' => "pictures#update"
-
-get 'pictures/:id' => 'pictures#show'
-
-delete 'pictures/:id' => 'pictures#destroy'
+resources :pictures
+resources :users, only: [:new, :create]
 end
